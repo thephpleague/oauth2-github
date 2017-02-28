b6649ab573216e517056f1bcf23e7293dfc5bf64
Steven Maguire <stevenmaguire@gmail.com>
2017-02-27 20:49:55 -0600

    Update scope test

--- test/src/Provider/GithubTest.php
+++ test/src/Provider/GithubTest.php
@@ -1,9 +1,12 @@
 <?php namespace League\OAuth2\Client\Test\Provider;
 
+use League\OAuth2\Client\Tool\QueryBuilderTrait;
 use Mockery as m;
 
 class GithubTest extends \PHPUnit_Framework_TestCase
 {
+    use QueryBuilderTrait;
+
     protected $provider;
 
     protected function setUp()
@@ -39,11 +42,13 @@ class GithubTest extends \PHPUnit_Framework_TestCase
 
     public function testScopes()
     {
-        $options = ['scope' => [uniqid(),uniqid()]];
-
+        $scopeSeparator = ',';
+        $options = ['scope' => [uniqid(), uniqid()]];
+        $query = ['scope' => implode($scopeSeparator, $options['scope'])];
         $url = $this->provider->getAuthorizationUrl($options);
+        $encodedScope = $this->buildQueryString($query);
+        $this->assertContains($encodedScope, $url);
 
-        $this->assertContains(urlencode(implode(',', $options['scope'])), $url);
     }
 
     public function testGetAuthorizationUrl()
